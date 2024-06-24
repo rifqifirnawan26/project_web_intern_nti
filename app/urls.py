8b788b3118a94cd7d54bda7647bfc268808355ee
rifqifirnawan26 <rifqi.firnawan20@gmail.com>
2024-06-24 14:07:11 +0700

    Updated views.py and dashboard.html for CPU utilization dashboard

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('metrics/', views.get_metrics, name='get_metrics'),
     path('memory_metrics/', views.get_memory_metrics, name='get_memory_metrics'),
+    path('metrics2/', views.get_metrics3, name='get_metrics3'),
 ]
 
