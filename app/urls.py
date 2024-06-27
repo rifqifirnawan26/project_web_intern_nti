8de0e75665e2fc2fba9caecedb182ef6be2ad176
rifqifirnawan26 <rifqi.firnawan20@gmail.com>
2024-06-28 01:53:50 +0700

    Menambahkan update terbaru pada top dan networktraffic

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('metrics/', views.get_metrics, name='get_metrics'),
     path('memory_metrics/', views.get_memory_metrics, name='get_memory_metrics'),
     path('metrics2/', views.get_metrics3, name='get_metrics3'),
+    path('metrics3/', views.get_metrics, name='get_metrics'),
+    path('cpu_metrics/', views.get_cpu_metrics, name='cpu_metrics'),
+    path('memory_metrics2/', views.get_memory_metrics, name='memory_metrics'),
+    path('disk1/', views.get_disk1, name='disk_read'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from influxdb_client import InfluxDBClient
 
 # URL, token, dan nama bucket untuk koneksi ke InfluxDB
 url = "http://192.168.9.16:8086"
-token = "huOa1-uFcQfnnouYBx8Yk_G4hA4CF0ASwfCMEN1VYmH6Cf4BS3ILYD1WJs2vGnRrfR7iThl4eouVAnz2EVLL-Q=="
+token = "V2JRMGtkEm6BuyXq6QIsjdgfTysg0OY-Zc2zM3v_P0OehjwQ_cmziiSwq8rP590I14yY6ktbxMcVDTirDpuoaw=="
 org = "interntt"
 bucket = "intern"
 
@@ -95,6 +95,51 @@ def get_latest_data2(time_range='1h'):
                 })
 
         return records
+    
+def get_cpu_metrics(time_range='1h'):
+   with InfluxDBClient(url=url, token=token, org=org, timeout=20_000) as client:
+        query = f"""
+            from(bucket: "{bucket}")
+            |> range(start: -{time_range})
+            |> filter(fn: (r) => r["_measurement"] == "system")
+            |> filter(fn: (r) => r["_field"] == "cpu")
+        """
+        tables = client.query_api().query(query, org=org)
+        records = []
+
+        for table in tables:
+            for record in table.records:
+                records.append({
+                    'host': record['host'],
+                    'value': record.get_value(),
+                    'timestamp': record.get_time().isoformat(),
+                })
+
+        return records
+   
+def get_disk_read(time_range='1h'):
+    with InfluxDBClient(url=url, token=token, org=org, timeout=20_000) as client:
+        query = f"""
+            from(bucket: "intern")
+            |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+            |> filter(fn: (r) => r["_measurement"] == "system")
+            |> filter(fn: (r) => r["_field"] == "diskread")
+        """
+
+        tables = client.query_api().query(query, org=org)
+        records = []
+
+        for table in tables:
+            for record in table.records:
+                records.append({
+                    'host': record['host'],
+                    'value': record.get_value(),
+                    'timestamp': record.get_time().isoformat(),
+                })
+
+        return records
+
+
 
 def calculate_percentage(data):
     percentages = {}
@@ -103,13 +148,15 @@ def calculate_percentage(data):
         total_records = len(records)
         
         if total_records == 0:
-            percentages[key] = {"percentage": 0, "host": records[0]['host']}
+            percentages[key] = {"percentage": 0, "host": records[0]['host'], "timestamp": records[0]['timestamp']}
         else:
             percentages[key] = {
                 "percentage": (total_value / total_records) * 100,
-                "host": records[0]['host']
+                "host": records[0]['host'],
+                "timestamp": records[0]['timestamp']
             }
     return percentages
+
 
 def calculate_percentage2(data):
     percentages = {}
@@ -137,6 +184,22 @@ def calculate_percentage3(data):
 
     host_percentages = {host: (data['total_value'] / data['total_records']) * 100 for host, data in host_data.items()}
     return host_percentages
+
+def calculate_percentage4(data):
+    percentages = {}
+    for key, records in data.items():
+        total_value = sum(record['field_value'] for record in records)
+        total_records = len(records)
+        
+        if total_records == 0:
+            percentages[key] = {"percentage": 0, "host": records[0]['host'], "timestamp": records[0]['timestamp']}
+        else:
+            percentages[key] = {
+                "percentage": (total_value / total_records) * 100,
+                "host": records[0]['host'],
+                "timestamp": records[0]['timestamp']
+            }
+    return percentages
 
 def get_metrics(request):
     time_range = request.GET.get('time_range', '1h')
@@ -191,6 +254,12 @@ def get_metrics3(request):
 
     return JsonResponse(result, safe=False)
 
+def get_disk1(request):
+    time_range = request.GET.get('time_range', '1h')
+    data = get_disk_read(time_range)
+    percentages = calculate_percentage4(data)
+    return JsonResponse(percentages, safe=False)
+
 def dashboard(request, time_range='1h'):
     data = get_latest_data(time_range)
     percentages = calculate_percentage(data)
@@ -199,6 +268,11 @@ def dashboard(request, time_range='1h'):
 def dashboard(request, time_range='1h'):
     data = get_latest_memory_data(time_range)
     percentages = calculate_percentage2(data)
+    return render(request, 'dashboard.html', {'data': percentages, 'time_range': time_range})
+
+def dashboard(request, time_range='1h'):
+    data = get_disk_read(time_range)
+    percentages = calculate_percentage4(data)
     return render(request, 'dashboard.html', {'data': percentages, 'time_range': time_range})
 
 def dashboard(request):
